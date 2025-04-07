48beba579580edbb76215217c23289af4766e6c6
Julia Kozuki <kozukijulia@gmail.com>
2025-04-07 15:19:26 -0300

    Incluido ordenação da lista

--- git.js
+++ git.js
@@ -1,4 +1,5 @@
 let comidas = [']lasanha' , 'Pizza', 'sushi', 'hamburguer']
 console.log(`lista de comidas: ${comidas}`);
 console.log(`Primeira comidas: ${comidas[0]}`);
-
+comidas.sort();
+console.log(`comidas ordenadas: ${comidas}`);	
